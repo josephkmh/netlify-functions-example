4d8ce43350051184ea264aa391d082f5316a4f62
Joey <jmarshmenthowell@gmail.com>
2020-11-13 11:17:08 +0100

    try different return format

--- src/lambda/profile.js
+++ src/lambda/profile.js
@@ -2,16 +2,18 @@ import fetch from "node-fetch";
 
 const API_ENDPOINT = "https://bwi.3yourmind.com/api/v2.0/my-profile/profile/";
 
-exports.handler = async (event, context) => {
+exports.handler = async (event, context, callback) => {
     return fetch(API_ENDPOINT, {
         headers: {
             Authorization: `Token 01a3c578766d54984790415c7b7c649241fbcc68`,
         },
     })
-        .then((response) => response.json())
-        .then((data) => ({
+        .then(response => callback(null, {
             statusCode: 200,
-            body: data,
-        }))
+            headers: {
+                "Content-type": "appplication/json"
+            },
+            body: response
+        })
         .catch((error) => ({ statusCode: 422, body: String(error) }));
 };
