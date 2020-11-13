d4103b22746814f3743a5613c6fbc349f3a74d20
Joey <jmarshmenthowell@gmail.com>
2020-11-13 11:29:09 +0100

    fix function after testing locally

--- src/lambda/profile.js
+++ src/lambda/profile.js
@@ -3,19 +3,19 @@ import fetch from "node-fetch";
 const API_ENDPOINT = "https://bwi.3yourmind.com/api/v2.0/my-profile/profile/";
 
 exports.handler = async (event, context, callback) => {
-    return fetch(API_ENDPOINT, {
-        headers: {
-            Authorization: `Token 01a3c578766d54984790415c7b7c649241fbcc68`,
-        },
-    })
-        .then((response) =>
-            callback(null, {
-                statusCode: 200,
-                headers: {
-                    "Content-type": "appplication/json",
-                },
-                body: response,
-            })
-        )
-        .catch((error) => ({ statusCode: 422, body: String(error) }));
+    try {
+        const profile = await fetch(API_ENDPOINT, {
+            headers: {
+                Authorization: `Token 01a3c578766d54984790415c7b7c649241fbcc68`,
+            },
+        });
+        const data = await profile.json();
+        callback(null, {
+            statusCode: 200,
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify(data),
+        });
+    } catch (error) {
+        callback(error);
+    }
 };
