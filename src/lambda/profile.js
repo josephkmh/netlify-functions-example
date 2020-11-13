fe4323f4095d6107003204ba09526c9834f50c4d
Joey <jmarshmenthowell@gmail.com>
2020-11-13 11:19:40 +0100

    fix typo

--- src/lambda/profile.js
+++ src/lambda/profile.js
@@ -8,12 +8,14 @@ exports.handler = async (event, context, callback) => {
             Authorization: `Token 01a3c578766d54984790415c7b7c649241fbcc68`,
         },
     })
-        .then(response => callback(null, {
-            statusCode: 200,
-            headers: {
-                "Content-type": "appplication/json"
-            },
-            body: response
-        })
+        .then((response) =>
+            callback(null, {
+                statusCode: 200,
+                headers: {
+                    "Content-type": "appplication/json",
+                },
+                body: response,
+            })
+        )
         .catch((error) => ({ statusCode: 422, body: String(error) }));
 };
